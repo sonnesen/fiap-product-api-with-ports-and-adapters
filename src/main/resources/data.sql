7ec16e692f8c740733c7955cb37e6e717e7b9492
Winston Spencer Sonnesen <sonnesen@gmail.com>
2025-07-14 23:13:26 -0300

    Add script to initialize the database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into categories (id, name, description, active, created_at, updated_at, deleted_at) values
+('a92d1f1f-5cb2-4c8d-a344-d732d280ba66', 'Category 1', 'Category 1 description', true, current_timestamp, current_timestamp, null),
+('4e16bd2f-9578-4acf-8468-c7e4367e8e7c', 'Category 2', 'Category 2 description', true, current_timestamp, current_timestamp, null),
+('d7a18c7b-7b59-4a13-a9b7-1dd3274b7ddd', 'Category 3', 'Category 3 description', true, current_timestamp, current_timestamp, null);
